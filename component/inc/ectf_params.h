5e4eb672c4640eaa1b9b7663443c5fcc3be488dc
Roshan <139107908+redhoodtxt@users.noreply.github.com>
2024-02-16 14:01:17 +0800

    Adding SMU1 Design

---
+++ component/inc/ectf_params.h
@@ -0,0 +1,8 @@
+#ifndef __ECTF_PARAMS__
+#define __ECTF_PARAMS__
+#define COMPONENT_ID 2003195177
+#define COMPONENT_BOOT_MSG "Component 2 boot"
+#define ATTESTATION_LOC "Virginia"
+#define ATTESTATION_DATE "01/24/24"
+#define ATTESTATION_CUSTOMER "Fritz"
+#endif
